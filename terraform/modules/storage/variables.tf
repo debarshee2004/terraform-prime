fb7ff8a21854c6b2f552ae1b9720a89c01238767
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2025-07-07 18:26:47 +0530

    Added a storage configurations file

---
+++ terraform/modules/storage/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  type        = string
+}
+
+variable "alb_zone_id" {
+  description = "Zone ID of the Application Load Balancer"
+  type        = string
+}
